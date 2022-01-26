7455a910301e34da863734c6242e235ae5ac8e09
Brando <leukocyte@protonmail.com>
2022-01-26 14:20:16 -0600

    initial commit, 1) and 2) completed

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -15,6 +15,11 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
